057fc4d9c187575bac1cc2691a85d3693abb6ce4
Youngdev96 <133093104+Youngdev96@users.noreply.github.com>
2023-07-17 17:36:47 +0900

    JS-weather

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,16 @@
+const API_KEY = "33cdaaf5541c10b8515fc3c10c8df1f3";
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lng = position.coords.longitude;
-  console.log("You live in", lat, lng);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+  fetch(url) //url 변수를 불러옴
+    .then((response) => response.json()) // url 변수에 있는 정보를 json에 넣음
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 function onGeoError() {
   alert("Can't find you. No weather for you");
